4cdba3bd16cea5657c73886db995c61b736dca21
alexlomu <91721507+alexlomu@users.noreply.github.com>
2022-12-07 14:13:44 +0100

    challenge 1 y 2

---
+++ main.py
@@ -0,0 +1,122 @@
+#Import all the libraries that are necessary
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+
+#Challenge 1
+
+#In this challenge we will use the Temp_States.csv file.
+#First import it into a data frame called temp.
+temp = pd.read_csv("Temp_States.csv")
+
+#Print temp
+print(temp)
+
+#Explore the data types of the Temp dataframe. What type of data do we have? Comment your result.
+temp.info()
+#The City and State columns are type object and the Temperature column is type float64
+
+#Select the rows where state is New York
+print(temp[temp.State=="New York"])
+
+#What is the average of the temperature of cities in New York?
+print(temp[temp.State=="New York"].mean())
+
+#We want to know cities and states with Temperature above 15 degrees Celcius
+print(temp[temp.Temperature > 15].City)
+
+#We want to know which cities have a temperature above 15 degrees Celcius and below 20 degrees Celcius
+print(temp[(temp.Temperature > 15) & (temp.Temperature < 20)].City)
+
+#Find the mean and the standard deviation of the temperature of each state
+print(temp.groupby("State").Temperature.mean())
+
+
+#Challenge 2
+
+#Load the employee.csv file into a DataFrame. Call the dataframe employee
+employee = pd.read_csv("Employee.csv")
+
+#Explore the data types of the Employee dataframe. Comment your results
+employee.info()
+#We have 5 columns with object type: Name, Department, Education, Gender, Title and 2 with int 64 data type: Years and Salary
+
+#Show visually the frequency distribution (histogram) of the employee dataset. In few words describe these histograms?
+sns.histplot(employee)
+#What's the average salary in this company?
+print(employee.Salary.mean())
+#What's the highest salary?
+print(employee.Salary.max())
+#What's the lowest salary?
+print(employee.Salary.min())
+#Who are the employees with the lowest salary?
+print(employee[employee.Salary == employee.Salary.min()])
+#Could you give all the information about an employee called David?
+print(employee[employee.Name == "David"])
+#Could you give only David's salary?
+print(employee[employee.Name == "David"].Salary)
+#Print all the rows where job title is associate
+print(employee[employee.Title == "associate"])
+#Print the first 3 rows of your dataframe
+print(employee[:3])
+#Find the employees who's title is associate and the salary above 55?
+print(employee[(employee.Title == "associate") & (employee.Salary > 55)])
+#Group the employees based on their number of years of employment. What are the average salaries in each group?
+print(employee.groupby("Years").Salary.mean())
+#What is the average Salary per title?
+print(employee.groupby("Title").Salary.mean())
+#Show a visual summary of the data using boxplot. What Are the First and Third Quartiles? Comment your results.
+sns.boxplot(data = employee)
+first_quart_years = np.percentile(employee.Years,25)
+third_quart_years = np.percentile(employee.Years,75)
+first_quart_salary = np.percentile(employee.Salary,25)
+third_quart_salary = np.percentile(employee.Salary,75)
+print(first_quart_years, third_quart_years, first_quart_salary, third_quart_salary)
+
+#Is the mean salary per gender different?
+print(employee.groupby(employee.Gender).Salary.mean())
+#Find the minimum, mean and the maximum of all numeric columns for each Department.
+print(employee.groupby(employee.Department).describe())
+#Bonus Question
+#For each department, compute the difference between the maximal salary and the minimal salary.
+
+
+#Challenge 3
+
+#Open the Orders.csv dataset. Name your dataset orders
+
+#Explore your dataset by looking at the data types and the summary statistics. Comment your results
+
+#What is the average Purchase Price?
+
+#What were the highest and lowest purchase prices?
+
+#Select all the customers we have in Spain
+
+#How many customers do we have in Spain?
+
+#Select all the customers who have bought more than 50 items ?
+
+#Select orders from Spain that are above 50 items
+
+#Select all free orders
+
+#Select all orders that are 'lunch bag'
+
+#Select all orders that are made in 2011 and are 'lunch bag'
+
+#Show the frequency distribution of the amount spent in Spain.
+
+#Select all orders made in the month of August
+
+#Select how many orders are made by countries in the month of August
+
+#What's the average amount of money spent by country
+
+#What's the most expensive item?
+
+#What was the average amount spent per year ?
+
+
+
